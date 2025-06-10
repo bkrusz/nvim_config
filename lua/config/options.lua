d07440bc8a5776a54ce400fbf5153d07e0732646
bkrusz <bkrusz@icloud.com>
2025-06-09 18:38:27 -0700

    fix: termguicolors

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_check_order = false
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true
